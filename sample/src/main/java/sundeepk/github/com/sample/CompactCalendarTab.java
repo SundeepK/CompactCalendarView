920a953a3e96cc6a68adb39b78cb45f954aaf7c1
SundeepK <SundeepK@users.noreply.github.com>
2016-06-22 13:31:17 +0100

    Refactor code from MainActivity sample to a separate tab to be used inside a viewpager.

---
+++ sample/src/main/java/sundeepk/github/com/sample/CompactCalendarTab.java
@@ -0,0 +1,172 @@
+package sundeepk.github.com.sample;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+
+import com.github.sundeepk.compactcalendarview.CompactCalendarView;
+import com.github.sundeepk.compactcalendarview.domain.Event;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+
+public class CompactCalendarTab extends Fragment {
+
+    private static final String TAG = "MainActivity";
+    private Calendar currentCalender = Calendar.getInstance(Locale.getDefault());
+    private SimpleDateFormat dateFormatForMonth = new SimpleDateFormat("MMM - yyyy", Locale.getDefault());
+    private boolean shouldShow = false;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v =inflater.inflate(R.layout.main_tab,container,false);
+
+        final List<String> mutableBookings = new ArrayList<>();
+
+        final ListView bookingsListView = (ListView) v.findViewById(R.id.bookings_listview);
+        final Button showPreviousMonthBut = (Button) v.findViewById(R.id.prev_button);
+        final Button showNextMonthBut = (Button) v.findViewById(R.id.next_button);
+        final Button slideCalendarBut = (Button) v.findViewById(R.id.slide_calendar);
+        final Button showCalendarWithAnimationBut = (Button) v.findViewById(R.id.show_with_animation_calendar);
+
+        final ArrayAdapter adapter = new ArrayAdapter<>(getContext(), android.R.layout.simple_list_item_1, mutableBookings);
+        bookingsListView.setAdapter(adapter);
+        final CompactCalendarView compactCalendarView = (CompactCalendarView) v.findViewById(R.id.compactcalendar_view);
+
+        // below allows you to configure color for the current day in the month
+        compactCalendarView.setCurrentDayBackgroundColor(getResources().getColor(R.color.black));
+        // below allows you to configure colors for the current day the user has selected
+        compactCalendarView.setCurrentSelectedDayBackgroundColor(getResources().getColor(R.color.dark_red));
+
+        addEvents(compactCalendarView, -1);
+        addEvents(compactCalendarView, Calendar.DECEMBER);
+        addEvents(compactCalendarView, Calendar.AUGUST);
+        compactCalendarView.invalidate();
+
+        // below line will display Sunday as the first day of the week
+        // compactCalendarView.setShouldShowMondayAsFirstDay(false);
+
+        //set initial title
+        final ActionBar toolbar = ((ActionBarActivity) getActivity()).getSupportActionBar();
+        toolbar.setTitle(dateFormatForMonth.format(compactCalendarView.getFirstDayOfCurrentMonth()));
+
+        //set title on calendar scroll
+        compactCalendarView.setListener(new CompactCalendarView.CompactCalendarViewListener() {
+            @Override
+            public void onDayClick(Date dateClicked) {
+                List<Event> bookingsFromMap = compactCalendarView.getEvents(dateClicked);
+                Log.d(TAG, "inside onclick " + dateClicked);
+                if(bookingsFromMap != null){
+                    Log.d(TAG, bookingsFromMap.toString());
+                    mutableBookings.clear();
+                    for(Event booking : bookingsFromMap){
+                        mutableBookings.add((String)booking.getData());
+                    }
+                    adapter.notifyDataSetChanged();
+                }
+
+            }
+
+            @Override
+            public void onMonthScroll(Date firstDayOfNewMonth) {
+                toolbar.setTitle(dateFormatForMonth.format(firstDayOfNewMonth));
+            }
+        });
+
+        showPreviousMonthBut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                compactCalendarView.showPreviousMonth();
+            }
+        });
+
+        showNextMonthBut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                compactCalendarView.showNextMonth();
+            }
+        });
+
+        slideCalendarBut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (shouldShow) {
+                    compactCalendarView.showCalendar();
+                } else {
+                    compactCalendarView.hideCalendar();
+                }
+                shouldShow = !shouldShow;
+            }
+        });
+
+        showCalendarWithAnimationBut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (shouldShow) {
+                    compactCalendarView.showCalendarWithAnimation();
+                } else {
+                    compactCalendarView.hideCalendarWithAnimation();
+                }
+                shouldShow = !shouldShow;
+            }
+        });
+
+        return v;
+    }
+
+    private void addEvents(CompactCalendarView compactCalendarView, int month) {
+        currentCalender.setTime(new Date());
+        currentCalender.set(Calendar.DAY_OF_MONTH, 1);
+        Date firstDayOfMonth = currentCalender.getTime();
+        for (int i = 0; i < 6; i++) {
+            currentCalender.setTime(firstDayOfMonth);
+            if (month > -1) {
+                currentCalender.set(Calendar.MONTH, month);
+            }
+            currentCalender.add(Calendar.DATE, i);
+            setToMidnight(currentCalender);
+            long timeInMillis = currentCalender.getTimeInMillis();
+
+            List<Event> events = getEvents(timeInMillis, i);
+
+            compactCalendarView.addEvents(events);
+        }
+    }
+
+    private List<Event> getEvents(long timeInMillis, int day) {
+        if (day < 2) {
+            return Arrays.asList(new Event(Color.argb(255, 169, 68, 65), timeInMillis, "Event at " + new Date(timeInMillis)));
+        } else if ( day > 2 && day <= 4) {
+            return Arrays.asList(
+                    new Event(Color.argb(255, 169, 68, 65), timeInMillis, "Event at " + new Date(timeInMillis)),
+                    new Event(Color.argb(255, 100, 68, 65), timeInMillis, "Event 2 at " + new Date(timeInMillis)));
+        } else {
+            return Arrays.asList(
+                    new Event(Color.argb(255, 169, 68, 65), timeInMillis, "Event at " + new Date(timeInMillis) ),
+                    new Event(Color.argb(255, 100, 68, 65), timeInMillis, "Event 2 at " + new Date(timeInMillis)),
+                    new Event(Color.argb(255, 70, 68, 65), timeInMillis, "Event 3 at " + new Date(timeInMillis)));
+        }
+    }
+
+    private void setToMidnight(Calendar calendar) {
+        calendar.set(Calendar.HOUR_OF_DAY, 0);
+        calendar.set(Calendar.MINUTE, 0);
+        calendar.set(Calendar.SECOND, 0);
+        calendar.set(Calendar.MILLISECOND, 0);
+    }
+}
---
+++ sample/src/main/java/sundeepk/github/com/sample/Tab2.java
@@ -0,0 +1,16 @@
+package sundeepk.github.com.sample;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Tab2 extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.tab_2,container,false);
+        return v;
+    }
+}
