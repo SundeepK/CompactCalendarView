9130e945b44806448fec40dca0d0e0fc9f5a8b28
SundeepK <SundeepK@users.noreply.github.com>
2016-06-22 13:36:44 +0100

    Update main activity to work with viewpager.

--- sample/src/main/java/sundeepk/github/com/sample/MainActivity.java
+++ sample/src/main/java/sundeepk/github/com/sample/MainActivity.java
@@ -1,172 +1,52 @@
 package sundeepk.github.com.sample;
 
-import android.graphics.Color;
 import android.os.Bundle;
-import android.support.v7.app.ActionBar;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBarActivity;
-import android.util.Log;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.ListView;
-
-import com.github.sundeepk.compactcalendarview.CompactCalendarView;
-import com.github.sundeepk.compactcalendarview.domain.Event;
-
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.List;
-import java.util.Locale;
 
 
 public class MainActivity extends ActionBarActivity {
 
-    private static final String TAG = "MainActivity";
-    private Calendar currentCalender = Calendar.getInstance(Locale.getDefault());
-    private SimpleDateFormat dateFormatForMonth = new SimpleDateFormat("MMM - yyyy", Locale.getDefault());
-    private boolean shouldShow = false;
+    private Toolbar toolbar;
+    private ViewPager pager;
+    private ViewPagerAdapter adapter;
+    private SlidingTabLayout tabs;
+    private CharSequence titles[]= {"Home","Events"};
+    private int numberOfTabs = 2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final ActionBar actionBar = getSupportActionBar();
-        final List<String> mutableBookings = new ArrayList<>();
+        toolbar = (Toolbar) findViewById(R.id.tool_bar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(true);
 
-        final ListView bookingsListView = (ListView) findViewById(R.id.bookings_listview);
-        final Button showPreviousMonthBut = (Button) findViewById(R.id.prev_button);
-        final Button showNextMonthBut = (Button) findViewById(R.id.next_button);
-        final Button slideCalendarBut = (Button) findViewById(R.id.slide_calendar);
-        final Button showCalendarWithAnimationBut = (Button) findViewById(R.id.show_with_animation_calendar);
+        // Creating The ViewPagerAdapter and Passing Fragment Manager, Titles fot the Tabs and Number Of Tabs.
+        adapter =  new ViewPagerAdapter(getSupportFragmentManager(), titles, numberOfTabs);
 
-        final ArrayAdapter adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, mutableBookings);
-        bookingsListView.setAdapter(adapter);
-        final CompactCalendarView compactCalendarView = (CompactCalendarView) findViewById(R.id.compactcalendar_view);
+        // Assigning ViewPager View and setting the adapter
+        pager = (ViewPager) findViewById(R.id.pager);
+        pager.setAdapter(adapter);
 
-        // below allows you to configure color for the current day in the month
-        compactCalendarView.setCurrentDayBackgroundColor(getResources().getColor(R.color.black));
-        // below allows you to configure colors for the current day the user has selected
-        compactCalendarView.setCurrentSelectedDayBackgroundColor(getResources().getColor(R.color.dark_red));
-        
-        addEvents(compactCalendarView, -1);
-        addEvents(compactCalendarView, Calendar.DECEMBER);
-        addEvents(compactCalendarView, Calendar.AUGUST);
-        compactCalendarView.invalidate();
+        // Assiging the Sliding Tab Layout View
+        tabs = (SlidingTabLayout) findViewById(R.id.tabs);
+        tabs.setDistributeEvenly(true); // To make the Tabs Fixed set this true, This makes the tabs Space Evenly in Available width
 
-        // below line will display Sunday as the first day of the week
-        // compactCalendarView.setShouldShowMondayAsFirstDay(false);
-
-        //set initial title
-        actionBar.setTitle(dateFormatForMonth.format(compactCalendarView.getFirstDayOfCurrentMonth()));
-
-        //set title on calendar scroll
-        compactCalendarView.setListener(new CompactCalendarView.CompactCalendarViewListener() {
+        // Setting Custom Color for the Scroll bar indicator of the Tab View
+        tabs.setCustomTabColorizer(new SlidingTabLayout.TabColorizer() {
             @Override
-            public void onDayClick(Date dateClicked) {
-                List<Event> bookingsFromMap = compactCalendarView.getEvents(dateClicked);
-                Log.d(TAG, "inside onclick " + dateClicked);
-                if(bookingsFromMap != null){
-                    Log.d(TAG, bookingsFromMap.toString());
-                    mutableBookings.clear();
-                    for(Event booking : bookingsFromMap){
-                        mutableBookings.add((String)booking.getData());
-                    }
-                    adapter.notifyDataSetChanged();
-                }
-
-            }
-
-        @Override
-        public void onMonthScroll(Date firstDayOfNewMonth) {
-                actionBar.setTitle(dateFormatForMonth.format(firstDayOfNewMonth));
+            public int getIndicatorColor(int position) {
+                return getResources().getColor(R.color.black);
             }
         });
 
-        showPreviousMonthBut.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-               compactCalendarView.showPreviousMonth();
-            }
-        });
-
-        showNextMonthBut.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                 compactCalendarView.showNextMonth();
-            }
-        });
-
-        slideCalendarBut.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (shouldShow) {
-                    compactCalendarView.showCalendar();
-                } else {
-                    compactCalendarView.hideCalendar();
-                }
-                shouldShow = !shouldShow;
-            }
-        });
-
-        showCalendarWithAnimationBut.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (shouldShow) {
-                    compactCalendarView.showCalendarWithAnimation();
-                } else {
-                    compactCalendarView.hideCalendarWithAnimation();
-                }
-                shouldShow = !shouldShow;
-            }
-        });
-
-    }
-
-    private void addEvents(CompactCalendarView compactCalendarView, int month) {
-        currentCalender.setTime(new Date());
-        currentCalender.set(Calendar.DAY_OF_MONTH, 1);
-        Date firstDayOfMonth = currentCalender.getTime();
-        for (int i = 0; i < 6; i++) {
-            currentCalender.setTime(firstDayOfMonth);
-            if (month > -1) {
-                currentCalender.set(Calendar.MONTH, month);
-            }
-            currentCalender.add(Calendar.DATE, i);
-            setToMidnight(currentCalender);
-            long timeInMillis = currentCalender.getTimeInMillis();
-
-            List<Event> events = getEvents(timeInMillis, i);
-
-            compactCalendarView.addEvents(events);
-        }
-    }
-
-    private List<Event> getEvents(long timeInMillis, int day) {
-        if (day < 2) {
-            return Arrays.asList(new Event(Color.argb(255, 169, 68, 65), timeInMillis, "Event at " + new Date(timeInMillis)));
-        } else if ( day > 2 && day <= 4) {
-            return Arrays.asList(
-                    new Event(Color.argb(255, 169, 68, 65), timeInMillis, "Event at " + new Date(timeInMillis)),
-                    new Event(Color.argb(255, 100, 68, 65), timeInMillis, "Event 2 at " + new Date(timeInMillis)));
-        } else {
-            return Arrays.asList(
-                    new Event(Color.argb(255, 169, 68, 65), timeInMillis, "Event at " + new Date(timeInMillis) ),
-                    new Event(Color.argb(255, 100, 68, 65), timeInMillis, "Event 2 at " + new Date(timeInMillis)),
-                    new Event(Color.argb(255, 70, 68, 65), timeInMillis, "Event 3 at " + new Date(timeInMillis)));
-        }
-    }
-
-    private void setToMidnight(Calendar calendar) {
-        calendar.set(Calendar.HOUR_OF_DAY, 0);
-        calendar.set(Calendar.MINUTE, 0);
-        calendar.set(Calendar.SECOND, 0);
-        calendar.set(Calendar.MILLISECOND, 0);
+        // Setting the ViewPager For the SlidingTabsLayout
+        tabs.setViewPager(pager);
     }
 
     @Override
