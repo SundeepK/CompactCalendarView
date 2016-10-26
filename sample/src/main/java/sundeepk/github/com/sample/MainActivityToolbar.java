39e1024345a6decd125225220ea5e4ae7db66f4c
SundeepK <SundeepK@users.noreply.github.com>
2016-10-26 14:15:37 +0100

    Bulk commit sample app changes to add collapsing toolbar with CompactCalendar inside. Needs clean up.

---
+++ sample/src/main/java/sundeepk/github/com/sample/MainActivityToolbar.java
@@ -0,0 +1,180 @@
+package sundeepk.github.com.sample;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.AppBarLayout;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.view.ViewCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.github.sundeepk.compactcalendarview.CompactCalendarView;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.TimeZone;
+
+public class MainActivityToolbar extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    private AppBarLayout mAppBarLayout;
+
+    private SimpleDateFormat dateFormat = new SimpleDateFormat("d MMMM yyyy", /*Locale.getDefault()*/Locale.ENGLISH);
+
+    private CompactCalendarView mCompactCalendarView;
+
+    private boolean isExpanded = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_toolbar);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        setTitle("Section 1");
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        mAppBarLayout = (AppBarLayout) findViewById(R.id.app_bar_layout);
+
+        // Set up the CompactCalendarView
+        mCompactCalendarView = (CompactCalendarView) findViewById(R.id.compactcalendar_view);
+
+        // Force English
+        mCompactCalendarView.setLocale(TimeZone.getDefault(), Locale.ENGLISH);
+
+        mCompactCalendarView.setShouldDrawDaysHeader(true);
+
+        mCompactCalendarView.setListener(new CompactCalendarView.CompactCalendarViewListener() {
+            @Override
+            public void onDayClick(Date dateClicked) {
+                setSubtitle(dateFormat.format(dateClicked));
+            }
+
+            @Override
+            public void onMonthScroll(Date firstDayOfNewMonth) {
+                setSubtitle(dateFormat.format(firstDayOfNewMonth));
+            }
+        });
+
+        // Set current date to today
+        setCurrentDate(new Date());
+
+        final ImageView arrow = (ImageView) findViewById(R.id.date_picker_arrow);
+
+        RelativeLayout datePickerButton = (RelativeLayout) findViewById(R.id.date_picker_button);
+
+        datePickerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (isExpanded) {
+                    ViewCompat.animate(arrow).rotation(0).start();
+                    mAppBarLayout.setExpanded(false, true);
+                    isExpanded = false;
+                } else {
+                    ViewCompat.animate(arrow).rotation(180).start();
+                    mAppBarLayout.setExpanded(true, true);
+                    isExpanded = true;
+                }
+            }
+        });
+    }
+
+    public void setCurrentDate(Date date) {
+        setSubtitle(dateFormat.format(date));
+        if (mCompactCalendarView != null) {
+            mCompactCalendarView.setCurrentDate(date);
+        }
+
+    }
+
+    @Override
+    public void setTitle(CharSequence title) {
+        TextView tvTitle = (TextView) findViewById(R.id.title);
+
+        if (tvTitle != null) {
+            tvTitle.setText(title);
+        }
+    }
+
+    public void setSubtitle(String subtitle) {
+        TextView datePickerTextView = (TextView) findViewById(R.id.date_picker_text_view);
+
+        if (datePickerTextView != null) {
+            datePickerTextView.setText(subtitle);
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_camera) {
+            // Handle the camera action
+        } else if (id == R.id.nav_gallery) {
+
+        } else if (id == R.id.nav_slideshow) {
+
+        } else if (id == R.id.nav_manage) {
+
+        } else if (id == R.id.nav_share) {
+
+        } else if (id == R.id.nav_send) {
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+}
